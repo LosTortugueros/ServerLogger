f8654a0d2e6725ddb47b73a1a034283a280ae328
Jules CANTEGRIL <julescantegril@MacBook-Pro-de-Jules-CANTEGRIL.local>
2014-12-05 03:28:20 +0100

    Use semaphore

---
+++ access.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_GET["user"]){
+  $user = $_GET["user"];
+  http_response_code($code);
+  $semRes = sem_get(SEM_KEY, 1, 0666, 0); 
+  if(sem_acquire($semRes)) {
+  	$file = file_get_contents($user.'.json');
+  	 sem_release($semRes); // release the semaphore so other process can use it
+    }
+  $data = json_decode($file);
+  unset($file);
+  header('Cache-Control: no-cache, must-revalidate');
+  header('Content-type: application/json');
+  echo $data;
+}
